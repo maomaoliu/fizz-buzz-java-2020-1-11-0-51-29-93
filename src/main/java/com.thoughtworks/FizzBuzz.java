0d19a6d93879c808d74d29e46f2dab6c9d5550e4
Liu Lu <yacht2005@gmail.com>
2020-01-11 23:54:41 +0800

    refactor: remove duplicate code -> use another way to organize code

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,47 +1,33 @@
 package com.thoughtworks;
 
 class FizzBuzz {
-    String fizzBuzz(int number) {
-        String numberString = String.valueOf(number);
-        if (numberString.contains("3") && !numberString.contains("5")) {
-            return "Fizz";
-        }
-        if (numberString.contains("5") && !numberString.contains("7")) {
-            String result = "Buzz";
-            result += getAddOnIfDividableBy7(number);
-            return result;
-        }
-        if (numberString.contains("7")) {
-            if (numberString.contains("3")) {
-                return "Fizz";
-            }
-            String result = "";
-            result += getAddOnIfDividableBy3(number);
-            result += getAddOnIfDividableBy7(number);
-            return result.equals("") ? numberString : result;
-        }
-
+    String fizzBuzz(int number){
         String result = "";
-        result = handleDividableNumber(number, result);
-        return result.equals("") ? numberString : result;
+        if(satisfyFizzCondition(number)){
+            result += "Fizz";
+        }
+        if(satisfyBuzzCondition(number)){
+            result += "Buzz";
+        }
+        if(satisfyWhizzCondition(number)){
+            result += "Whizz";
+        }
+        return result.equals("") ? String.valueOf(number) : result;
     }
 
-    private String handleDividableNumber(int number, String result) {
-        result += getAddOnIfDividableBy3(number);
-        result += getAddOnIfDividableBy5(number);
-        result += getAddOnIfDividableBy7(number);
-        return result;
+    private boolean satisfyFizzCondition(int number) {
+        return (!isContains(number, "5") || isContains(number, "7")) && (isContains(number, "3") || number % 3 == 0);
     }
 
-    private String getAddOnIfDividableBy3(int number) {
-        return number % 3 == 0 ? "Fizz" : "";
+    private boolean satisfyBuzzCondition(int number) {
+        return !isContains(number, "7") && (isContains(number, "5") || (!isContains(number, "3") && number % 5 == 0));
     }
 
-    private String getAddOnIfDividableBy5(int number) {
-        return number % 5 == 0 ? "Buzz" : "";
+    private boolean satisfyWhizzCondition(int number) {
+        return (!isContains(number, "3") || (isContains(number, "5") && !isContains(number, "7"))) && number % 7 == 0;
     }
 
-    private String getAddOnIfDividableBy7(int number) {
-        return number % 7 == 0 ? "Whizz" : "";
+    private boolean isContains(int number, String string) {
+        return String.valueOf(number).contains(string);
     }
 }
