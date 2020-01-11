5e720552d3a4c5b6564afedadb134bec1ad16cd6
Liu Lu <yacht2005@gmail.com>
2020-01-11 22:14:57 +0800

    refactor test, to use parameterized test

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -1,18 +1,30 @@
 package com.thoughtworks;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class FizzBuzzTest {
 
     private FizzBuzz fizzBuzz;
 
-    @Test
-    public void test_number_division(){
+    @ParameterizedTest
+    @MethodSource("numberAndResultProvider")
+    public void test_number_division(int number, String result){
         fizzBuzz = new FizzBuzz();
-        assertEquals("Fizz", fizzBuzz.fizzBuzz(3));
-        assertEquals("Buzz", fizzBuzz.fizzBuzz(5));
-        assertEquals("Whizz", fizzBuzz.fizzBuzz(7));
+        assertEquals(result, fizzBuzz.fizzBuzz(number));
+    }
+
+    public static Stream<Arguments> numberAndResultProvider() {
+        return Stream.of(
+                arguments(3, "Fizz"),
+                arguments(5, "Buzz"),
+                arguments(7, "Whizz")
+        );
     }
 }
