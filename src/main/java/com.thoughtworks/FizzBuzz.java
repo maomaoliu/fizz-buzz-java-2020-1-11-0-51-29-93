8a8cea23430b68e51c5236e78d3d517f76c289c9
Liu Lu <yacht2005@gmail.com>
2020-01-13 14:44:41 +0800

    refactor: make condition smaller

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,32 +2,43 @@ package com.thoughtworks;
 
 class FizzBuzz {
     String fizzBuzz(int number){
-        String result = "";
         if(satisfyFizzCondition(number)){
+            return "Fizz";
+        }
+        String result = "";
+        if(satisfyCombinedFizzCondition(number)){
             result += "Fizz";
         }
-        if(satisfyBuzzCondition(number)){
+        if(satisfyCombinedBuzzCondition(number)){
             result += "Buzz";
         }
-        if(satisfyWhizzCondition(number)){
+        if(satisfyCombinedWhizzCondition(number)){
             result += "Whizz";
         }
         return result.equals("") ? String.valueOf(number) : result;
     }
 
     private boolean satisfyFizzCondition(int number) {
-        return (!isContains(number, "5") || isContains(number, "7")) && (isContains(number, "3") || number % 3 == 0);
+        return isContains(number, "3") && (!isContains(number, "5") || isContains(number, "7"));
     }
 
-    private boolean satisfyBuzzCondition(int number) {
-        return !isContains(number, "7") && (isContains(number, "5") || (!isContains(number, "3") && number % 5 == 0));
+    private boolean satisfyCombinedFizzCondition(int number) {
+        return isDividableBy(number, 3) && (!isContains(number, "5") || isContains(number, "7"));
     }
 
-    private boolean satisfyWhizzCondition(int number) {
-        return (!isContains(number, "3") || (isContains(number, "5") && !isContains(number, "7"))) && number % 7 == 0;
+    private boolean satisfyCombinedBuzzCondition(int number) {
+        return !isContains(number, "7") && (isContains(number, "5") || isDividableBy(number, 5));
     }
 
-    private boolean isContains(int number, String string) {
-        return String.valueOf(number).contains(string);
+    private boolean satisfyCombinedWhizzCondition(int number) {
+        return isDividableBy(number, 7);
+    }
+
+    private boolean isDividableBy(int number, int divisor) {
+        return number % divisor == 0;
+    }
+
+    private boolean isContains(int number, String subNumber) {
+        return String.valueOf(number).contains(subNumber);
     }
 }
